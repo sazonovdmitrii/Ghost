c62034b12a014f2efa33d9e6454d02cad29d9d7d
root <root@kvmnl01-6036.fornex.org>
2014-08-15 18:20:29 +0400

    change logo

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://185.18.52.51',
+        url: 'http://127.0.0.1/',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -59,7 +59,7 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '185.18.52.51',
+            host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         }
